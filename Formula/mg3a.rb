65316a48228f94233b40a7300e93c11593296ef2
paaguti <paaguti@gmail.com>
2017-12-23 17:47:52 +0100

    Clean up of the formula

--- Formula/mg3a.rb
+++ Formula/mg3a.rb
@@ -1,32 +1,29 @@
 class Mg3a < Formula
-  desc "Small Emacs-like editor inspired by mg with UTF8 support"
-  homepage "http://www.bengtl.net/files/mg3a/"
-  url "http://www.bengtl.net/files/mg3a/mg3a.161017.tar.gz"
-  sha256 "e97951850f9755e5f754e46f5d198b9d99a5b55a2a67deb0b9159f22b4ff39be"
+  desc "Small Emacs-like editor inspired by mg with UTF8 support."
+  homepage "https://gtihub.com/paaguti/mg3a/"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "e257022f63f28ed08c7302fa1fd53ec1dd473280927b6cbea5a7c22e077c4ab2" => :sierra
-    sha256 "dd3e30d9e1b1cf19e3ec54414a295b155d908dd30e76c728b53e91f72b43d1c2" => :el_capitan
-    sha256 "f2f3e4ca89f9c90b57a47ecd7d5a29ab674da7bc9341fdc31b09a09bf99de372" => :yosemite
-  end
+  url "https://github.com/paaguti/mg3a/archive/20171130.tar.gz"
+  sha256 "03b147806b184b79ffbbabe4b870225bce24c681b3d1e955f22e1c78d9b82885"
 
-  option "with-c-mode", "Include the original C mode"
-  option "with-clike-mode", "Include the C mode that also handles Perl and Java"
-  option "with-python-mode", "Include the Python mode"
-  option "with-most", "Include c-like and python modes, user modes and user macros"
-  option "with-all", "Include all fancy stuff"
+  option "with-all",  "Include all fancy stuff (build with -DALL)"
 
   conflicts_with "mg", :because => "both install `mg`"
+
+  patch do
+    url "https://raw.githubusercontent.com/paaguti/mg3a/20171130/debian/patches/02-Makefile.bsd"
+    sha256 "d040cd15d86bb34382023ae3cf85a33770585baebb13a3d91b100a72f46e890b"
+  end
+
+  patch do
+    url "https://raw.githubusercontent.com/paaguti/mg3a/20171130/debian/patches/01-pipein.diff"
+    sha256 "e7cbf41772d9f748bc54978846342d1309bc35810cf6a2b73d4dce4b9c501a5b"
+  end
 
   def install
     if build.with?("all")
       mg3aopts = %w[-DALL]
     else
-      mg3aopts = %w[-DDIRED -DPREFIXREGION -DUSER_MODES -DUSER_MACROS]
-      mg3aopts << "-DLANGMODE_C" if build.with?("c-mode")
-      mg3aopts << "-DLANGMODE_PYTHON" if build.with?("python-mode") || build.with?("most")
-      mg3aopts << "-DLANGMODE_CLIKE" if build.with?("clike-mode") || build.with?("most")
+      mg3aopts = %w[-DDIRED -DPREFIXREGION -DSEARCHALL -DPIPEIN -DUSER_MODES -DUSER_MACROS -DLANGMODE_PYTHON -DLANGMODE_CLIKE]
     end
 
     system "make", "CDEFS=#{mg3aopts * " "}", "LIBS=-lncurses", "COPT=-O3"
@@ -36,7 +33,7 @@ class Mg3a < Formula
   end
 
   test do
-    (testpath/"command.sh").write <<-EOS.undent
+    (testpath/"command.sh").write <<~EOS
       #!/usr/bin/expect -f
       set timeout -1
       spawn #{bin}/mg
